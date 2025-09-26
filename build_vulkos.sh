4088c43bdcadffeab15c06a4811c2194f58bc544
l8bloom <l8bloomapi@gmail.com>
2025-09-26 08:49:49 +0200

    feat: add cmake to project

--- build_vulkos.sh
+++ build_vulkos.sh
@@ -1,12 +1,23 @@
 #! /bin/bash
 
-clang++ --std=c++23 main.cpp \
-    -I/home/dom-ak45/Downloads/glfw/include \
-    -L/home/dom-ak45/Downloads/glfw/build/src \
-    -l glfw \
-    -I/home/dom-ak45/Downloads/vulkanSDK/1.4.321.1/x86_64/include \
-    -L/home/dom-ak45/Downloads/vulkanSDK/1.4.321.1/x86_64/lib \
-    -l vulkan \
-    -o build/vulkos \
+set -e
+
+# to not lose it:
+# cmake -S . -B build -D CMAKE_C_COMPILER=clang -D CMAKE_CXX_COMPILER=clang++ -D CMAKE_PREFIX_PATH="$HOME/.local;$HOME/Downloads/vulkanSDK/1.4.321.1/x86_64" -G Ninja
+# GLFW:
+# cmake -S . -B build -D CMAKE_C_COMPILER=clang -D CMAKE_INSTALL_PREFIX="$HOME/.local" -D GLFW_BUILD_X11=0 -D BUILD_SHARED_LIBS=ON -G Ninja
+
+clang-tidy main.cpp
+
+# this is not really needed to run every time, but doesn't matter
+cmake \
+    -S . \
+    -B build \
+    -D CMAKE_C_COMPILER=clang \
+    -D CMAKE_CXX_COMPILER=clang++ \
+    -D CMAKE_PREFIX_PATH="$HOME/.local;$HOME/Downloads/vulkanSDK/1.4.321.1/x86_64" \
+    -G Ninja
+
+ninja -C build
 
 ./build/vulkos

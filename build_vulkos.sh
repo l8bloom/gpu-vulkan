91d7eb6bac961b46c799e57af424a77f202f815e
l8bloom <l8bloomapi@gmail.com>
2025-10-03 21:02:02 +0200

    feat: add release build option

--- build_vulkos.sh
+++ build_vulkos.sh
@@ -10,14 +10,28 @@ set -e
 clang-tidy main.cpp
 
 # this is not really needed to run every time, but doesn't matter
-cmake \
-    -S . \
-    -B build \
-    -D CMAKE_C_COMPILER=clang \
-    -D CMAKE_CXX_COMPILER=clang++ \
-    -D CMAKE_PREFIX_PATH="$HOME/.local;$HOME/Downloads/vulkanSDK/1.4.321.1/x86_64" \
-    -D SHADER_SLANG_SOURCES="shader.slang" \
-    -G Ninja
+if [[ "$1" == "release" ]]; then
+    rm -rf ./build
+    cmake \
+	-S . \
+	-B build \
+	-D CMAKE_C_COMPILER=clang \
+	-D CMAKE_CXX_COMPILER=clang++ \
+	-D CMAKE_PREFIX_PATH="$HOME/.local;$HOME/Downloads/vulkanSDK/1.4.321.1/x86_64" \
+	-D CMAKE_BUILD_TYPE=Release \
+	-D SHADER_SLANG_SOURCES="shader.slang" \
+	-G Ninja
+else
+    rm -rf ./build
+    cmake \
+        -S . \
+	-B build \
+	-D CMAKE_C_COMPILER=clang \
+	-D CMAKE_CXX_COMPILER=clang++ \
+	-D CMAKE_PREFIX_PATH="$HOME/.local;$HOME/Downloads/vulkanSDK/1.4.321.1/x86_64" \
+	-D SHADER_SLANG_SOURCES="shader.slang" \
+	-G Ninja
+fi
 
 ninja -C build
 

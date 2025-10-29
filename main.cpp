9e117a6bf4efb5a012dd335162a5ccbe13547c54
l8bloom <l8bloomapi@gmail.com>
2025-10-29 11:56:49 +0100

    feat: add stb headers

--- main.cpp
+++ main.cpp
@@ -35,6 +35,9 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
 constexpr uint32_t WINDOW_WIDTH = 2560;
 constexpr uint32_t WINDOW_HEIGHT = 1440;
 
